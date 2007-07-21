0815a9af0972bf5f351de2159088d8b091d46882
wilsontp <wilsontp@1244f836-552d-0410-882b-4589cda7d6c5>
2007-07-21 07:14:05 +0000

    mouse code is working, but cursor is hidden when it should be a text entry cursor.

--- Server/NetWorker.cs
+++ Server/NetWorker.cs
@@ -6,22 +6,29 @@ using System.Net.Sockets;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 
-namespace CR.RightClick.Server {
-  public enum EventTypeEnum : byte {
+namespace CR.RightClick.Server
+{
+  public enum EventTypeEnum : byte
+  {
     MouseMove,  // bytes 4-7=x, bytes 8-11=y
     MouseDown,  // byte 4=button
     MouseUp,    // byte 4=button
     KeyDown,    // bytes 4-7=keycode
-    KeyUp       // bytes 4-7=keycode
+    KeyUp,      // bytes 4-7=keycode
+    MouseWheel
   }
 
-  class NetWorker {
+  class NetWorker
+  {
+    public bool Done = false;
+
     private const UInt32 MOUSEEVENTF_LEFTDOWN = 0x0002;
     private const UInt32 MOUSEEVENTF_LEFTUP = 0x0004;
     private const UInt32 MOUSEEVENTF_RIGHTDOWN = 0x0008;
     private const UInt32 MOUSEEVENTF_RIGHTUP = 0x0010;
     private const UInt32 MOUSEEVENTF_MIDDLEDOWN = 0x0020;
     private const UInt32 MOUSEEVENTF_MIDDLEUP = 0x0040;
+    private const UInt32 MOUSEEVENTF_WHEEL = 0x0800;
     UInt32[] mouseDownItems = {
       MOUSEEVENTF_LEFTDOWN, MOUSEEVENTF_RIGHTDOWN, MOUSEEVENTF_MIDDLEDOWN};
     UInt32[] mouseUpItems = {
@@ -53,12 +60,12 @@ namespace CR.RightClick.Server {
     /// Send a mouse up and mouse down event.
     /// </summary>
     /// <param name="button">0-left, 1-right</param>
-    public void SendMouseDown(int button) {
+    public void SendMouseDown(int button)
+    {
       mouse_event(mouseDownItems[button], 0, 0, 0, new System.IntPtr());
-      mouse_event(mouseUpItems[button], 0, 0, 0, new System.IntPtr());
     }
-    public void SendMouseUp(int button) {
-      mouse_event(mouseDownItems[button], 0, 0, 0, new System.IntPtr());
+    public void SendMouseUp(int button)
+    {
       mouse_event(mouseUpItems[button], 0, 0, 0, new System.IntPtr());
     }
 
@@ -67,34 +74,44 @@ namespace CR.RightClick.Server {
        Properties.Settings.Default.port);
 
     ServerWindow MainWindow;
-    public void Start(ServerWindow MainWindow) {
+    public void Start(ServerWindow MainWindow)
+    {
       this.MainWindow = MainWindow;
       System.Net.IPEndPoint endPoint = new System.Net.IPEndPoint(
         IPAddress.Any, Properties.Settings.Default.port);
 
       System.Diagnostics.Debug.WriteLine("Thread started");
 
-      while (true) {
-        if (socket.Available > 0) {
+      while (true)
+      {
+        if (socket.Available > 0)
+        {
           byte[] b = socket.Receive(ref endPoint);
-          System.Diagnostics.Debug.WriteLine("Data received: " + b[0].ToString());
-          switch ((EventTypeEnum)b[0]) {
+          //System.Diagnostics.Debug.WriteLine("Data received: " + b[0].ToString());
+          int x, y;
+          switch ((EventTypeEnum)b[0])
+          {
             case EventTypeEnum.MouseMove:
-              int x = BitConverter.ToInt32(b, 4);
-              int y = BitConverter.ToInt32(b, 8);
-              Cursor.Position = new System.Drawing.Point(x, y);
+              x = BitConverter.ToInt32(b, 4);
+              y = BitConverter.ToInt32(b, 8);
+              mouse_event(0x8001, (uint)x, (uint)y, 0, IntPtr.Zero);
               break;
             case EventTypeEnum.MouseDown:
-              int buttonDown = BitConverter.ToInt32(b, 4);
-              SendMouseDown(buttonDown);
+              x = BitConverter.ToInt32(b, 4);
+              SendMouseDown(x);
               break;
             case EventTypeEnum.MouseUp:
-              int buttonUp = BitConverter.ToInt32(b, 4);
-              SendMouseUp(buttonUp);
+              x = BitConverter.ToInt32(b, 4);
+              SendMouseUp(x);
+              break;
+            case EventTypeEnum.MouseWheel:
+              x = BitConverter.ToInt32(b, 4);
+              y = BitConverter.ToInt32(b, 8);
+              mouse_event(0x800, 640, 512, (uint)y, IntPtr.Zero);
               break;
           }
         }
-        System.Threading.Thread.Sleep(0);
+        System.Threading.Thread.Sleep(30);
       }
     }
   }
